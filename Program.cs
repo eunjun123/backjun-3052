b40017b920d931e85aded426f793d88e1162cde3
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 19:26:50 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+    class MainApp
+    {
+        static void Main()
+        {
+            int[] inputNum = new int[10];
+            bool[] isCheck = new bool[43];
+
+            int count = 0;
+
+            for (int i = 0; i < inputNum.Length; i++)
+            {
+                inputNum[i] = int.Parse(ReadLine()) % 42;
+                isCheck[inputNum[i]] = true;
+            }
+
+            for (int i = 0; i < isCheck.Length; i++)
+            {
+                if (isCheck[i]) count++;
+            }
+            WriteLine(count);
+        }
+    }
